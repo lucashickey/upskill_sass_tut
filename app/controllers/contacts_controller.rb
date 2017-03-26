d0163341971b948ee8f9690b26bcc8067b941e4a
Lucas Hickey <lucashasstuff@gmail.com>
2017-03-26 11:48:32 +0000

    added errors for unfilled fields

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,22 @@
 class ContactsController < ApplicationController
-    def new
-      @contact = Contact.new
-    end
-    
-    def create
-      @contact = Contact.new(contact_params) #<-- this sets the instance variable
-      if @contact.save
-        redirect_to new_contact_path, notice: "message sent."
-      else 
-        redirect_to new_contact_path, notice: "error occured."
-    end
-end
-
-private
-  def contact_params  
-    params.require(:contact).permit(:name, :email, :comments)
+  def new
+    @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params) #<-- this sets the instance variable
+    if @contact.save
+      flash[:success] = "Message Sent."
+      redirect_to new_contact_path
+    else 
+      flash[:error] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
+    end
+  end
+
+
+  private
+    def contact_params  
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end

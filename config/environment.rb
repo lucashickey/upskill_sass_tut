e45a294265d238d1674052b756498bd1a1f3f9aa
Lucas Hickey <lucashasstuff@gmail.com>
2017-04-01 02:01:38 +0000

    f

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,6 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :authentication => :plain
   :domain         => 'obscure-plains-70361.herokuapp.com'
-  
-}
+  }
 ActionMailer::Base.delivery_method = :smtp

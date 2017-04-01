33bd30a4f53c0049b68c21bd17577e3c36ca72c3
Lucas Hickey <lucashasstuff@gmail.com>
2017-04-01 01:46:09 +0000

    fuck this too

---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'obscure-plains-70361.herokuapp.com'
+}
+
+
+ActionMailer::Base.delivery_method = :smtp

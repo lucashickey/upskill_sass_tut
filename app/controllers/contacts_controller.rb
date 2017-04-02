1c2b99bedef7f540955c79c6386389a63b4de7e7
Lucas Hickey <lucashasstuff@gmail.com>
2017-04-02 05:35:56 +0000

    mailer fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params) #<-- this sets the instance variable
     if @contact.save
       name = params[:contact][:name]
-      email = params[:email]
+      email = params[:contact][:email]
       body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver #<--this is the action for actually sending the email
-      default from: 'lucas.b.hickey@hotmail.com'
       flash[:success] = "Message Sent."
       redirect_to new_contact_path
     else 
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,6 @@ class ContactMailer < ActionMailer::Base
     @email = email
     @body = body
     
-    mail(from: email, subject: "contact form message")
+    mail(from: email, subject: 'Contact Form Message')
   end 
 end
